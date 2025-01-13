e0472f0cc83ceff091477ec80f5f6bd957ebf7f3
Samthewebguy <reachsamthewebguy@gmail.com>
2025-01-13 18:15:26 +0100

    I make sure the mobile nav works well

--- central.js
+++ central.js
@@ -1,17 +1,27 @@
 function showMobileNav(){
     const mobileNav = document.querySelector('.mobileNav');
     mobileNav.style.display=('flex');
+
     const MobileNavCloseIcon = document.querySelector('.MobileNavCloseIcon')
     MobileNavCloseIcon.style.display=('flex')
+
     const mobileHamburger = document.querySelector('.mobileHamburger')
     mobileHamburger.style.display=('none')
+
+    const startButton = document.querySelector('#startButton')
+    startButton.style.display=('none')
 }
 
 function hideMobileNav(){
     const mobileNav = document.querySelector('.mobileNav');
     mobileNav.style.display=('none')
+    
     const mobileHamburger = document.querySelector('.mobileHamburger')
     mobileHamburger.style.display=('flex')
+
     const MobileNavCloseIcon = document.querySelector('.MobileNavCloseIcon')
     MobileNavCloseIcon.style.display=('none')
+
+    const startButton = document.querySelector('#startButton')
+    startButton.style.display=('flex')
 }
